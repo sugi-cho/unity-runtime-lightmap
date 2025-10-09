a3911fd3d6dafca7235a76ec0c897018a4fad572
sugino <git@sugi.cc>
2025-10-09 18:46:34 +0900

    Add sample scene and related assets for demo: main.unity, materials, shadergraph, TexturePropSetter

---
+++ Assets/RealTimeLightBaker/Scripts/TexturePropSetter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TexturePropSetter : MonoBehaviour
+{
+    [SerializeField] string propertyName = "_MainTex";
+    MaterialPropertyBlock _mpb;
+    Renderer _renderer;
+    public Texture texture
+    {
+        set
+        {
+            _mpb ??= new MaterialPropertyBlock();
+            _renderer ??= GetComponent<Renderer>();
+            _renderer.GetPropertyBlock(_mpb);
+            _mpb.SetTexture(propertyName, value);
+            _renderer.SetPropertyBlock(_mpb);
+        }
+    }
+}
