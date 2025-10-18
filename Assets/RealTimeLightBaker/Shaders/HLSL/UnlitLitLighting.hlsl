d5da1e0f93937b8e37bc686c6ba3c09dd3cb9a07
sugino <git@sugi.cc>
2025-10-19 03:32:41 +0900

    Add specular support to UnlitLit lighting

--- Assets/RealTimeLightBaker/Shaders/HLSL/UnlitLitLighting.hlsl
+++ Assets/RealTimeLightBaker/Shaders/HLSL/UnlitLitLighting.hlsl
@@ -13,12 +13,25 @@ struct UnlitLitInput
 {
     float3 positionWS;
     float3 normalWS;
+    float3 viewDirWS;
+    float smoothness;
+    float specularStrength;
     float3 baseColor;
     float occlusion;
 };
 
-static float3 ComputeMainLightContribution(in UnlitLitInput inputData)
+struct LightingResult
 {
+    float3 diffuse;
+    float3 specular;
+};
+
+static LightingResult ComputeMainLightContribution(in UnlitLitInput inputData)
+{
+    LightingResult result;
+    result.diffuse = 0.0f;
+    result.specular = 0.0f;
+
     Light mainLight = GetMainLight();
 
     float4 shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
@@ -28,19 +41,37 @@ static float3 ComputeMainLightContribution(in UnlitLitInput inputData)
         float realtimeShadow = 1.0f;
     #endif
 
+    float smoothness = saturate(inputData.smoothness);
+    float shininess = exp2(10.0f * smoothness + 1.0f);
+
     float NdotL = saturate(dot(inputData.normalWS, mainLight.direction));
     float attenuation = mainLight.distanceAttenuation * mainLight.shadowAttenuation * realtimeShadow;
 
-    return mainLight.color * NdotL * attenuation;
+    float3 lightContribution = mainLight.color * attenuation;
+    result.diffuse = lightContribution * NdotL;
+
+    if (NdotL > 0.0f)
+    {
+        float3 halfDir = normalize(mainLight.direction + inputData.viewDirWS);
+        float specularTerm = pow(saturate(dot(inputData.normalWS, halfDir)), shininess);
+        result.specular = lightContribution * specularTerm * inputData.specularStrength;
+    }
+
+    return result;
 }
 
-static float3 ComputeAdditionalLightsContribution(in UnlitLitInput inputData)
+static LightingResult ComputeAdditionalLightsContribution(in UnlitLitInput inputData)
 {
-    float3 lighting = 0.0f;
+    LightingResult result;
+    result.diffuse = 0.0f;
+    result.specular = 0.0f;
+
     uint count = GetAdditionalLightsCount();
+    float smoothness = saturate(inputData.smoothness);
+    float shininess = exp2(10.0f * smoothness + 1.0f);
 
     [loop]
-    for (uint perObj = 0u; perObj < GetAdditionalLightsCount(); ++perObj)
+    for (uint perObj = 0u; perObj < count; ++perObj)
     {
         Light light = GetAdditionalLight(perObj, inputData.positionWS);
         float NdotL = saturate(dot(inputData.normalWS, light.direction));
@@ -53,27 +84,43 @@ static float3 ComputeAdditionalLightsContribution(in UnlitLitInput inputData)
         #endif
 
         float attenuation = light.distanceAttenuation * min(light.shadowAttenuation, realtimeShadow);
-        lighting += light.color * NdotL * attenuation;
+        float3 lightContribution = light.color * attenuation;
+        result.diffuse += lightContribution * NdotL;
+
+        if (NdotL > 0.0f)
+        {
+            float3 halfDir = normalize(light.direction + inputData.viewDirWS);
+            float specularTerm = pow(saturate(dot(inputData.normalWS, halfDir)), shininess);
+            result.specular += lightContribution * specularTerm * inputData.specularStrength;
+        }
     }
 
-    return lighting;
+    return result;
 }
 
-void UnlitLitLighting_float(float3 positionWS, float3 normalWS, float3 albedo, float occlusion, out float3 color)
+void UnlitLitLighting_float(float3 positionWS, float3 normalWS, float3 albedo, float occlusion, float smoothness, float specularStrength, out float3 color)
 {
     UnlitLitInput inputData;
     inputData.positionWS = positionWS;
     inputData.normalWS = normalize(normalWS);
+    inputData.viewDirWS = normalize(GetWorldSpaceViewDir(positionWS));
+    inputData.smoothness = saturate(smoothness);
+    inputData.specularStrength = max(specularStrength, 0.0f);
     inputData.baseColor = albedo;
     inputData.occlusion = occlusion;
 
-    float3 directLighting = ComputeMainLightContribution(inputData);
-    directLighting += ComputeAdditionalLightsContribution(inputData);
+    LightingResult mainLight = ComputeMainLightContribution(inputData);
+    LightingResult additionalLights = ComputeAdditionalLightsContribution(inputData);
 
+    float3 directLighting = mainLight.diffuse + additionalLights.diffuse;
     float3 indirectLighting = SampleSH(inputData.normalWS);
+    float3 specularLighting = mainLight.specular + additionalLights.specular;
 
-    float3 combinedLighting = (directLighting + indirectLighting) * max(inputData.occlusion, 0.0f);
-    color = inputData.baseColor * combinedLighting;
+    float occlusionFactor = max(inputData.occlusion, 0.0f);
+    float3 combinedLighting = (directLighting + indirectLighting) * occlusionFactor;
+    specularLighting *= occlusionFactor;
+
+    color = inputData.baseColor * combinedLighting + specularLighting;
 }
 
 #endif // UNLIT_LIT_LIGHTING_INCLUDED
