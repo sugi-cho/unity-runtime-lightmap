352a0e0ddf2cfd26bd46b7c3b14bfe4df9489afc
sugino <git@sugi.cc>
2025-10-20 23:25:15 +0900

    整理した

---
+++ Assets/Samples/Real Time Light Baker/0.1.0/Demo Scene/Scripts/TexturePropSetter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class TexturePropSetter : MonoBehaviour
+{
+    [SerializeField] private string propertyName = "_MainTex";
+    [SerializeField] private Renderer targetRenderer;
+
+    private MaterialPropertyBlock _mpb;
+
+    public Texture texture
+    {
+        set
+        {
+            if (!EnsureRendererAvailable())
+            {
+                return;
+            }
+
+            _mpb ??= new MaterialPropertyBlock();
+            targetRenderer.GetPropertyBlock(_mpb);
+
+            if (value != null)
+            {
+                _mpb.SetTexture(propertyName, value);
+            }
+            else
+            {
+                _mpb.SetTexture(propertyName, null);
+            }
+
+            targetRenderer.SetPropertyBlock(_mpb);
+        }
+    }
+
+    private bool EnsureRendererAvailable()
+    {
+        if (targetRenderer != null)
+        {
+            return true;
+        }
+
+        targetRenderer = GetComponent<Renderer>();
+        if (targetRenderer != null)
+        {
+            return true;
+        }
+
+        Debug.LogWarning($"{nameof(TexturePropSetter)} on '{name}' requires a Renderer reference.", this);
+        return false;
+    }
+}
